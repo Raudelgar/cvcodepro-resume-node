97a7f96a753245a958a5bdf0c5245ee8e49fb9ec
raudelgar <raudel24@gmail.com>
2020-07-06 23:01:26 -0400

    adding new contact service

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const cvrApp = express();
 const messagesRoute = require('./routes/api/v1/messages.js');
 const usersRoute = require('./routes/api/v1/users.js');
+const contactRoute = require('./routes/api/v1/contact.js');
 
 //MIddlewares
 cvrApp.use(express.json({ limit: '50mb' }));
@@ -14,5 +15,6 @@ cvrApp.use(cors());
 cvrApp.use('/new-messages', messagesRoute);
 cvrApp.use('/users', usersRoute);
 cvrApp.use('/create-new-user', usersRoute);
+cvrApp.use('/contact', contactRoute);
 
 exports.cvrApp = functions.https.onRequest(cvrApp);
